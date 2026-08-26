52804d41afe01c9c570b4b036ad6d3c5f58f66b5
wenting2110 <wenting2110@gmail.com>
2026-08-26 22:00:51 +0800

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.py
@@ -0,0 +1,10 @@
+# sorting: O(n^2 * logn)
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()
+            cur = stones.pop() - stones.pop()
+            if cur:
+                stones.append(cur)
+            
+        return stones[0] if stones else 0
