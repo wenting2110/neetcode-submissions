7a26a4b531c21ae3b9cbc9463d447cad19bc3eec
wenting2110 <wenting2110@gmail.com>
2026-08-26 22:26:42 +0800

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.py
@@ -0,0 +1,25 @@
+# binary search: O(n^2)
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        n = len(stones)
+
+        while n > 1:
+            cur = stones.pop() - stones.pop()
+            n -= 2
+            if cur > 0:
+                l, r = 0, n
+                while l < r:
+                    mid = (l + r) // 2
+                    if stones[mid] < cur:
+                        l = mid + 1
+                    else:
+                        r = mid
+                pos = l
+                n += 1
+                stones.append(0)
+                for i in range(n - 1, pos, -1):
+                    stones[i] = stones[i - 1]
+                stones[pos] = cur
+
+        return stones[0] if n > 0 else 0
